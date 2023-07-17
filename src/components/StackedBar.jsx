3ceb572c3a87ac1585488502b603495323d07f22
Mikael Barros <mikaelhjfb@gmail.com>
2023-07-17 18:58:07 -0300

    fix: ajusta dados do componente StackedBar (chart)

--- src/components/StackedBar.jsx
+++ src/components/StackedBar.jsx
@@ -12,28 +12,34 @@ export default function StackedBar() {
         const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
         const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
         const data = {
-            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            labels: ['2023', '2022', '2021', '2020', '2019'],
             datasets: [
-                {
-                    type: 'bar',
-                    label: 'Dataset 1',
-                    backgroundColor: documentStyle.getPropertyValue('--blue-500'),
-                    data: [50, 25, 12, 48, 90, 76, 42]
-                },
-                {
-                    type: 'bar',
-                    label: 'Dataset 2',
-                    backgroundColor: documentStyle.getPropertyValue('--green-500'),
-                    data: [21, 84, 24, 75, 37, 65, 34]
-                },
-                {
-                    type: 'bar',
-                    label: 'Dataset 3',
-                    backgroundColor: documentStyle.getPropertyValue('--yellow-500'),
-                    data: [41, 52, 24, 74, 23, 21, 32]
-                }
-            ]
-        };
+              {
+                type: 'bar',
+                label: 'A1',
+                backgroundColor: '#4dc9f6',
+                data: [9, 33, 30, 26, 17],
+              },
+              {
+                type: 'bar',
+                label: 'A2',
+                backgroundColor: '#f67019',
+                data: [0, 8, 13, 17, 6],
+              },
+              {
+                type: 'bar',
+                label: 'A3',
+                backgroundColor: '#537bc4',
+                data: [12, 26, 24, 46, 20],
+              },
+              {
+                type: 'bar',
+                label: 'A4',
+                backgroundColor: '#acc236',
+                data: [0, 30, 49, 25, 55],
+              },
+            ],
+          };
         const options = {
             maintainAspectRatio: false,
             aspectRatio: 0.8,
