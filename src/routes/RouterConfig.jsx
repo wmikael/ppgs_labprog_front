0e7d8eece9b4749b778a8a3d71819e63680d3ac7
Mikael Barros <mikaelhjfb@gmail.com>
2023-07-18 18:13:58 -0300

    fix: ajusta router após apagar componentes teste

--- src/routes/RouterConfig.jsx
+++ src/routes/RouterConfig.jsx
@@ -1,8 +1,6 @@
 import { Routes, Route } from "react-router-dom";
-import { Exemplo } from "../pages/Exemplo";
 import { DefaultLayout } from "../layouts/DefaultLayout";
 import { Programa } from "../pages/Programa/Programa";
-import { TesteDataTable } from "../pages/TesteDataTable";
 import { Docente } from "../pages/Docente/Docente";
 import { LoginPage } from "../pages/LoginPage/LoginPage";
 
@@ -12,9 +10,7 @@ export function RouterConfig() {
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/programa" element={<Programa />} />
-        <Route path="/teste-data-table" element={<TesteDataTable />} />
         <Route path="/docente" element={<Docente />} />
-        <Route path="/exemplo" element={<Exemplo />} />
       </Route>
     </Routes>
   );
