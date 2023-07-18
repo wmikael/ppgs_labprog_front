eabc1e27588f109d751185804043bed4af29497b
Mikael Barros <mikaelhjfb@gmail.com>
2023-07-18 01:18:19 -0300

    feat: implementacao do DocenteTable

--- src/pages/Programa/Programa.jsx
+++ src/pages/Programa/Programa.jsx
@@ -2,38 +2,15 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import "./Programa.css";
 import FiltrosPrograma from "../../components/FiltrosPrograma";
-import StackedBar from "../../components/StackedBar";
-import { DataTableComponent } from "../../components/DataTableComponent";
+import DocenteTable from "../../components/DocenteTable";
 
 export function Programa() {
-  const data = [
-    {
-      name: "Rupert",
-      age: 48,
-      city: "Austin",
-      country: "USA",
-      occupation: "Writer",
-      salary: 6200,
-      experience: 13,
-    },
-    {
-      name: "Sybil",
-      age: 29,
-      city: "Jacksonville",
-      country: "USA",
-      occupation: "Painter",
-      salary: 3900,
-      experience: 3,
-    },
-  ];
-
   const [programas, setProgramas] = useState([]);
   const [anoIni, setAnoIni] = useState("");
   const [anoFim, setAnoFim] = useState("");
   const [dadosTabela, setDadosTabela] = useState([]);
 
   useEffect(() => {
-    // Buscar os programas quando o componente for montado
     axios
       .get("http://localhost:8080/api/programa/all-programas")
       .then((response) => {
@@ -43,31 +20,27 @@ export function Programa() {
   }, []);
 
   const handleFiltrosChange = (programaSelecionado, anoIni, anoFim) => {
-    // Atualizar os valores dos filtros
     setAnoIni(anoIni);
     setAnoFim(anoFim);
+  };
 
-    // Chamada do Axios para buscar os dados da tabela com base nos filtros
+  const handlePesquisar = (anoIni, anoFim) => {
+    console.log("Ano Inicial:", anoIni);
+    console.log("Ano Final:", anoFim);
+  
+    if (!anoIni || !anoFim || anoIni.trim() === '' || anoFim.trim() === '') {
+      console.error("Ano inicial e ano final devem ser informados");
+      return;
+    }
+  
     axios
-      .get("http://localhost:8080/api/programa/obterDadosTabela", {
-        params: {
-          programa: programaSelecionado,
-          anoIni,
-          anoFim,
-        },
-      })
+      .get(`http://localhost:8080/api/Docente/obterProducoesQualis/${anoIni}/${anoFim}`)
       .then((response) => {
-        // Atualizar os dados da tabela com a resposta da API
         setDadosTabela(response.data);
       })
       .catch((error) => console.error(error));
   };
-
-  const header = (
-    <div className="flex flex-wrap align-items-center justify-content-between gap-2">
-      <span className="text-xl text-900 font-bold">Products</span>
-    </div>
-  );
+  
 
   return (
     <div>
@@ -78,16 +51,12 @@ export function Programa() {
           anoFim={anoFim}
           onFiltrosChange={handleFiltrosChange}
         />
-      </div>
-
-      <div>
-        <h4>Chart:</h4>
-        <StackedBar />
+        <button onClick={() => handlePesquisar(anoIni, anoFim)}>Pesquisar</button>
       </div>
 
       <div>
         <h4>Table:</h4>
-        <DataTableComponent data={data} header={header} />
+        <DocenteTable data={dadosTabela} />
       </div>
     </div>
   );
