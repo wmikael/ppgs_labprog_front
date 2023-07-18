6e253b3af9d55b6d18eaabe327e90fe082bda142
Mikael Barros <mikaelhjfb@gmail.com>
2023-07-18 17:58:41 -0300

    fix: ajusta RouterConfig para a page Docente

--- src/routes/RouterConfig.jsx
+++ src/routes/RouterConfig.jsx
@@ -3,7 +3,7 @@ import { Exemplo } from "../pages/Exemplo";
 import { DefaultLayout } from "../layouts/DefaultLayout";
 import { Programa } from "../pages/Programa/Programa";
 import { TesteDataTable } from "../pages/TesteDataTable";
-import { Docente } from "../pages/Docente";
+import { Docente } from "../pages/Docente/Docente";
 import { LoginPage } from "../pages/LoginPage/LoginPage";
 
 export function RouterConfig() {
