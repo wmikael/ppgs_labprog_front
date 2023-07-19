89be3b1bcd8a673371288c078f3c2b6895e540e0
Mikael Barros <mikaelhjfb@gmail.com>
2023-07-18 23:45:08 -0300

    feat: configura rotas para nos novos submenus

--- src/routes/RouterConfig.jsx
+++ src/routes/RouterConfig.jsx
@@ -3,6 +3,8 @@ import { DefaultLayout } from "../layouts/DefaultLayout";
 import { Programa } from "../pages/Programa/Programa";
 import { Docente } from "../pages/Docente/Docente";
 import { LoginPage } from "../pages/LoginPage/LoginPage";
+import { Producoes } from "../pages/Producoes/Producoes"
+import { Tecnicas } from "../pages/Tecnicas/Tecnicas"
 
 export function RouterConfig() {
   return (
@@ -10,6 +12,9 @@ export function RouterConfig() {
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/programa" element={<Programa />} />
+        <Route path="/producoes" element={<Producoes />} />
+        <Route path="/tecnicas" element={<Tecnicas />} />
+
         <Route path="/docente" element={<Docente />} />
       </Route>
     </Routes>
